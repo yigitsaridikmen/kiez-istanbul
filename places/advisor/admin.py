fe22d892094c051f06614047093ebb16dff32f1c
yigidos <saridikmenyigit@gmail.com>
2024-01-23 00:01:51 +0300

    Default model for places info is set

--- places/advisor/admin.py
+++ places/advisor/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import PlaceInfo
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = [
+    'user',    
+    'name',
+    'address',
+    'city',
+    'county',
+    'district',
+    'place_type',
+    'activity',
+    'largegroup',
+    'alone',
+    'dateable',
+    'price_category',
+    'kitchen',
+    'smoke',
+    'door_policy',
+    'loudness',
+    'comment' ,
+    'created_at'
+    ]
+    search_fields = [    
+    'name',
+    'city',
+    'county',
+    'district',
+    'place_type',
+    'activity',
+    'price_category',
+    ]
+
+admin.site.register(PlaceInfo,PlaceAdmin)
+
+
+
+
+
--- places/advisor/models.py
+++ places/advisor/models.py
@@ -1,3 +1,57 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class PlaceInfo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    address = models.TextField(blank=True)
+    city = models.CharField(max_length=100,default='Istanbul')
+    county = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+    TYPE_CHOICES = [
+        ('Bar', 'Bar'),
+        ('Club', 'Club'),
+        ('Cafe', 'Cafe'),
+        ('Food', 'Food'),
+    ]    
+    place_type = models.CharField(
+        max_length=10,
+        choices=TYPE_CHOICES, # You can set a default value if needed
+    )
+
+    activity = models.CharField(max_length=50)
+    
+    largegroup = models.BooleanField()
+    alone = models.BooleanField()
+    dateable = models.BooleanField()
+
+    PRICE_CHOICES = [
+        ('Cheap', 'Cheap'),
+        ('Normal', 'Normal'),
+        ('Expensive', 'Expensive'),
+    ]
+
+    price_category = models.CharField(
+        max_length=10,
+        choices=PRICE_CHOICES,
+        default='Normal',  # You can set a default value if needed
+    )
+    kitchen = models.BooleanField()
+    smoke = models.BooleanField()
+    door_policy = models.BooleanField()
+    LOUDNESS_CHOICES = [
+        ('Silent', 'Silent'),
+        ('Moderate', 'Moderate'),
+        ('Loud', 'Loud'),
+    ]    
+    loudness = models.CharField(
+        max_length=10,
+        choices=LOUDNESS_CHOICES,
+        default='Moderate',  # You can set a default value if needed
+    )
+    comment = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # def __str__(self):
+    #     return f'{self.user.username} - {self.name}'
