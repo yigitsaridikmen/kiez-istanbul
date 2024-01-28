38b6541838f3d9d002bd8ba78ee4624834b423aa
yigidos <saridikmenyigit@gmail.com>
2024-01-29 00:30:45 +0300

    Filters are added but not set

--- places/advisor/views.py
+++ places/advisor/views.py
@@ -16,8 +16,14 @@ def home_page(request, *args, **kwargs):
 @login_required(login_url='auth/login/')
 def list_places(request):
     qs = PlaceInfo.objects.all()
+	# Get distinct counties and districts
+    distinct_counties = PlaceInfo.objects.values_list('county', flat=True).distinct()
+    distinct_districts = PlaceInfo.objects.values_list('district', flat=True).distinct()
+
     context={
         "object_list":qs,
+		"distinct_counties":distinct_counties,
+		"distinct_districts":distinct_districts,
     }
     
     return render(request, "advisor/advisorhome.html", context=context)
